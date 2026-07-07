d96ac9797c59317e9a576107b4035616e0fb8750
Skyler Hart <112896057+skyler-hart@users.noreply.github.com>
2026-07-07 12:17:09 -0400

    Add pipeline function example

---
+++ docs/standards/powershell/examples/PipelineFunction.ps1
@@ -0,0 +1,41 @@
+function Get-KeldorComputerThing {
+    <#
+    .SYNOPSIS
+        Gets a Keldor computer thing.
+
+    .DESCRIPTION
+        Demonstrates a pipeline-aware ComputerName pattern for Keldor PowerShell commands.
+
+    .PARAMETER ComputerName
+        Specifies one or more computer names.
+
+    .EXAMPLE
+        'SERVER01' | Get-KeldorComputerThing
+
+        Gets a Keldor computer thing for SERVER01.
+
+    .OUTPUTS
+        Keldor.Computer.Thing
+
+    .LINK
+        https://docs.keldor.dev/powershell/keldor/Get-KeldorComputerThing
+    #>
+    [CmdletBinding(HelpUri = 'https://docs.keldor.dev/powershell/keldor/Get-KeldorComputerThing')]
+    param(
+        [Parameter(Position = 0, ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)]
+        [Alias('Host', 'Name', 'Computer', 'CN')]
+        [ValidateNotNullOrEmpty()]
+        [string[]]$ComputerName = $env:COMPUTERNAME
+    )
+
+    process {
+        foreach ($Computer in $ComputerName) {
+            [pscustomobject]@{
+                PSTypeName   = 'Keldor.Computer.Thing'
+                ComputerName = $Computer
+                Status       = 'Unknown'
+                Timestamp    = Get-Date
+            }
+        }
+    }
+}
